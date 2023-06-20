f32e686c68ca1e9210833642d85ff357a0f3c7bc
Arthur <arthurdedsec@gmail.com>
2023-06-19 21:11:21 -0300

    feito

--- controllers/logar.php
+++ controllers/logar.php
@@ -2,6 +2,22 @@
 
 include ('../controllers/conexao.php');
 
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    if ($email === 'admin@admin.com' && $senha ==='admin') {
+        $_SESSION['admin'] = true;
+        header("Location: ../views/admin.php");
+        echo "Login de admin realizado";
+        exit();
+    }
+}
+
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
---
+++ views/admin.php
@@ -0,0 +1,52 @@
+<?php
+include('../controllers/conexao.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/admin.css">
+    <title>Registros</title>
+</head>
+
+<body>
+
+<section id="Formulario">
+<?php
+
+
+
+
+
+    $sql = "SELECT * FROM usuarios";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+         
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>Nome</th>";
+            echo "<th>Email</th>";
+            echo "<th>Endereço</th>";
+            echo "</tr>";
+
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['nome'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "<td>" . $row['endereco'] . "</td>";
+            echo "</tr>";
+        }
+    } else {
+        echo "nenhum resultado encontrado";
+    }
+    ?>
+    </section>
+</body>
+
+</html>
